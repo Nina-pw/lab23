bf0a0a8e83cc6e653982a8dc18e5c9af8fc72e06
Pimmada <nina21551@gmail.com>
2023-03-03 15:09:39 +0700

    average OKAY

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
 
+int main(int argc, char*argv[]){
+    double sum = 0;
+    double average;
+    if(argc > 1){
+        for(int i =0; i < argc; i++){
+            sum += atof(argv[i]);
+        }
+        average = sum/(argc-1);
+        cout << "---------------------------------\n";
+        cout << "Average of " << argc-1 << " numbers = " << average << endl;
+        cout << "---------------------------------\n";
+    }else{
+        cout << "Please input numbers to find average.";
+    }
+}  
